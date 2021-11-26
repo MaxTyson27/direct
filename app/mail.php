1351eca327acfdc68788aae37b3183d244216bc3
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2021-11-26 17:28:27 +0300

    added PC version

--- app/mail.php
+++ app/mail.php
@@ -1,10 +1,13 @@
 <?php
 
-$recepient = "ad.web.site@yandex.ru";
+// $recepient = "ad.web.site@yandex.ru";
+$recepient = "iosmax@vk.ru";
 $siteName = "Директ.сайт";
 
 $phone = trim($_POST["phone"]);
-$message = "Телефон: $phone";
+$name = trim($_POST["name"]);
+$message = "Телефон: $phone" \n 
+"Имя: $name";
 
 $pagetitle = "Заявка с сайта \"$siteName\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
