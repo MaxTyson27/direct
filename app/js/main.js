803852759df88b3388287c02260d39d8a6062d9a
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2021-11-28 18:04:55 +0300

    added scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,40 @@
 $(function () {
 
+  $('.input-name').keypress(function (e) {
+    if (e.keyCode == 32) return false;
+    if (this.value.length == 1) {
+      this.value = this.value.toUpperCase();
+    }
+  });
 
+  $('.input-phone').inputmask({ "mask": "+7(999) 999-99-99" });
+
+
+  $(".menu a, .footer__menu a, .instructions__link, .instructions__list-link").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top - 75;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+  // $('.footer-top__title').on('click', function () {
+  //   $(this).next().slideToggle();
+  //   $(this).toggleClass('active');
+  // });
+
+  $('.advance__link, .price__link, .services__link').on('click', function(e){
+    e.preventDefault()
+    $('.popup').addClass('popup--active')
+    $('.popup-close').addClass('popup-close--active')
+  })
+  $('.popup-close').on('click', function(){
+    $(this).removeClass('popup-close--active')
+    $('.popup').removeClass('popup--active')
+  })
+  $('.popup__close').on('click', function(){
+    $('.popup-close').removeClass('popup-close--active')
+    $('.popup').removeClass('popup--active')
+  })
 });
 
 var link = document.querySelector(".different__link"),
